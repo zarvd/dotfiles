6c668cffa51d5822c8983ed1f346c05d179b345e
Jialun Cai <jialun.cai@pm.me>
2024-12-19 10:15:17 +1100

    Add get_lan_ip func

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("Cascadia Mono"),
-	-- font = wezterm.font("FiraCode Nerd Font Mono"),
+	font = wezterm.font("Cascadia Code"),
 	font_size = 13.0,
-	-- color_scheme = "Earthsong (Gogh)",
 	color_scheme = "VSCodeDark+ (Gogh)",
 }
