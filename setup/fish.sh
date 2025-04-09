72fb6d75d05c53d5efc27fde1efa1e60a5064615
Jialun Cai <jialun.cai@pm.me>
2025-04-09 21:36:35 +0800

    Fix path

--- setup/fish.sh
+++ setup/fish.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source "$CUR_DIR/helper.sh"
+source "setup/helper.sh"
 
 function setup_fish() {
   print_section "Setup Fish"
--- setup/git.sh
+++ setup/git.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source "$CUR_DIR/helper.sh"
+source "setup/helper.sh"
 
 function setup_git() {
   print_section "Setup Git"
